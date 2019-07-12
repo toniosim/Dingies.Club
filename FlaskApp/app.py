6ebf574a547decc3d58c8f1994d060f594bfd93c
toniosim <toniothepowerful@gmail.com>
2019-07-11 23:30:56 -0500

    added back HSSB, making it it's own separate page

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -15,6 +15,31 @@ def main():
 	#passing to index
 	return render_template("index.html", app_title=app_title, author=author)
 
+#hearthstone stoundboard
+@app.route("/hssb")
+def hssb():
+	card_conn = sqlite3.connect("cards.db")
+	sound_conn = sqlite3.connect("cardsounds.db")
+
+	card_conn.row_factory = sqlite3.Row
+	sound_conn.row_factory = sqlite3.Row
+
+
+	card_cur = card_conn.cursor()
+	sound_cur = sound_conn.cursor()
+
+	#getting all cards and soundfiles
+	card_cur.execute("SELECT * FROM cards ORDER BY cardname ASC")
+	sound_cur.execute("SELECT * FROM sounds ORDER BY cardname ASC")
+
+	card_rows = card_cur.fetchall()
+	sound_rows = sound_cur.fetchall()
+
+	card_conn.close()
+	sound_conn.close()
+
+	#passing to index
+	return render_template("hssb.html", app_title=app_title, author=author, card_rows=card_rows, sound_rows=sound_rows)
 
 if __name__ == "__main__":
 	app.run(debug=False)
