103eb1753547b342fa33614aa44b02d8a4c775a1
toniosim <tonio.simonetta@gmail.com>
2022-03-22 22:37:23 -0500

    removed HSSB

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,9 +1,7 @@
 #main application to run the soundboard
 #run and point browser to localhost:5000
 
-from flask import Flask, render_template, request, redirect, g
-import sqlite3
-import os, sys
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app_title = "Dingies Club"
@@ -20,31 +18,5 @@ def main():
 def about():
 	return render_template("about.html", app_title="About", author=author)
 
-#hearthstone stoundboard
-@app.route("/hssb")
-def hssb():
-	card_conn = sqlite3.connect("cards.db")
-	sound_conn = sqlite3.connect("cardsounds.db")
-
-	card_conn.row_factory = sqlite3.Row
-	sound_conn.row_factory = sqlite3.Row
-
-
-	card_cur = card_conn.cursor()
-	sound_cur = sound_conn.cursor()
-
-	#getting all cards and soundfiles
-	card_cur.execute("SELECT * FROM cards ORDER BY cardname ASC")
-	sound_cur.execute("SELECT * FROM sounds ORDER BY cardname ASC")
-
-	card_rows = card_cur.fetchall()
-	sound_rows = sound_cur.fetchall()
-
-	card_conn.close()
-	sound_conn.close()
-
-	#passing to index
-	return render_template("hssb.html", app_title="HSSB", author=author, card_rows=card_rows, sound_rows=sound_rows)
-
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+	app.run(debug=True)
