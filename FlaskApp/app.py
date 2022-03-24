27a55173b148a38d5925459e4d82870324942468
toniosim <tonio.simonetta@gmail.com>
2022-03-23 21:49:04 -0500

    added url_for and own css

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,7 +1,7 @@
 #main application to run the soundboard
 #run and point browser to localhost:5000
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app_title = "Dingies Club"
